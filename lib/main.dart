9bbe247ef844825545bde0ebce5ec35b81a30529
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-05 09:16:56 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,29 @@
+import 'dart:async';
+
+import 'app.dart';
 import 'index.dart';
 
-void main() => Global.init().then((e) => runApp(MyApp()));
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Fimber.w("system error:$details");
+  };
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Fimber.d("begin to render app.");
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ThemeModel>.value(value: ThemeModel()),
-        ChangeNotifierProvider<UserModel>.value(value: UserModel()),
-        ChangeNotifierProvider<LocaleModel>.value(value: LocaleModel()),
-      ],
-      child: Consumer2<ThemeModel, LocaleModel>(
-        builder: (BuildContext context, themeModel, localeModel, Widget child) {
-          Fimber.d("begin to print ThemeModel and LocaleModel.");
-
-          return MaterialApp(
-            theme: ThemeData(
-              primarySwatch: themeModel.theme,
-            ),
-            onGenerateTitle: (context) {
-              return GmLocalizations.of(context).title;
-            },
-            home: HomeRoute(),
-            locale: localeModel.getLocale(),
-            supportedLocales: [
-              const Locale('en', 'US'),
-              const Locale('zh', 'CN'),
-            ],
-            localizationsDelegates: [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GmLocalizationsDelegate(),
-            ],
-            localeResolutionCallback:
-                (Locale _locale, Iterable<Locale> supportedLocales) {
-              if (localeModel.getLocale() != null) {
-                return localeModel.getLocale();
-              } else {
-                Locale locale;
-                if (supportedLocales.contains(_locale)) {
-                  locale = _locale;
-                } else {
-                  locale = Locale('en', 'US');
-                }
-                return locale;
-              }
-            },
-
-            // register route
-            routes: <String, WidgetBuilder>{
-              ROUTE_LOGIN: (context) => LoginRoute(),
-              ROUTE_THEMES: (context) => ThemeChangeRoute(),
-              ROUTE_LANGUAGE: (context) => LanguageRoute(),
-            },
-          );
-        },
-      ),
-    );
-  }
+  runZoned(
+    () {
+//    ErrorWidget.builder = (FlutterErrorDetails details) {
+//      Zone.current.handleUncaughtError(details.exception, details.stack);
+//      return ErrorPage(details.exception.toString());
+//    };
+      runApp(MyApp());
+    },
+    onError: (Object obj, StackTrace stack) {
+      Fimber.w("run app exception:$obj");
+      Fimber.w("run app exception stack: $stack");
+    },
+    zoneSpecification: ZoneSpecification(
+      print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
+        Fimber.w("Zone:$self, ZoneDelegate:$parent, line:$line");
+      },
+    ),
+  );
 }
