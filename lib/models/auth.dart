638f6cfbf7c6768b29387942884e8a5c089ffdbd
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-10 21:55:12 +0800

    Update auth.dart

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:fimber/fimber.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:gogithub/utils/global.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uni_links/uni_links.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'account.dart';
 
@@ -17,4 +23,55 @@ class AuthModel with ChangeNotifier {
   }
 
   String get token => activeAccount.token;
+
+  _addAccount(Account account) async {
+    _accounts = [...accounts, account];
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(StorageKeys.accounts, json.encode(_accounts));
+  }
+
+  removeAccount(int index) async {
+    if (activeAccountIndex == index) {
+      activeAccountIndex = null;
+    }
+    _accounts.removeAt(index);
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(StorageKeys.accounts, json.encode(_accounts));
+    notifyListeners();
+  }
+
+  Future<void> _onSchemeDetected(Uri uri) async {
+    await closeWebView();
+
+    loading = true;
+    notifyListeners();
+
+    // get token by code
+  }
+
+  Future<void> init() async {
+    _sub = getUriLinksStream().listen(_onSchemeDetected, onError: (err) {
+      Fimber.e('getUriLinkStream failed', ex: err);
+    });
+
+    var prefs = await SharedPreferences.getInstance();
+    try {
+      String str = prefs.getString(StorageKeys.accounts);
+      Fimber.d('read accounts :$str');
+      _accounts = (json.decode(str ?? '[]') as List)
+          .map((item) => Account.fromJson(item))
+          .toList();
+    } catch (err) {
+      Fimber.w('prefs getAccount from cache failed', ex: err);
+      _accounts = [];
+    }
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _sub.cancel();
+    super.dispose();
+  }
 }
