7d72b71949b04efcf6e0df8779140c9755aa90e2
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-12 19:45:22 +0800

    Create home.dart

---
+++ lib/widgets/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
