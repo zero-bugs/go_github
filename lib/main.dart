8e20e21b50b66027db3e93491efe4d31cb046116
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-12 19:45:42 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
-void main() {}
+import 'package:fimber/fimber.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:gogithub/models/auth.dart';
+import 'package:gogithub/models/code.dart';
+import 'package:gogithub/models/notification.dart';
+import 'package:gogithub/models/theme.dart';
+
+import 'app.dart';
+
+void main() async {
+  Fimber.plantTree(DebugTree());
+
+  final notification = NotificationModel();
+  final themeModel = ThemeModel();
+  final authModel = AuthModel();
+  final codeModel = CodeModel();
+
+  await Future.wait([
+    themeModel.init(),
+    authModel.init(),
+    codeModel.init(),
+  ]);
+
+  runApp(MyApp());
+}
