90438b46d6ebfd3d733c1286e53f0d9a01441379
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-21 22:51:28 +0800

    Create settings.dart

---
+++ lib/screens/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
