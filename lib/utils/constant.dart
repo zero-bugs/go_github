26168a01b83fcaca510ff64328a5f77904700acc
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-21 22:40:22 +0800

    Update constant.dart

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,5 @@
-final String apiGtBaseUrl = 'https://api.github.com';
+final String gtApiBaseUrl = 'https://api.github.com';
 final String gtHomeDomain = 'https://github.com';
 final String thirdAuthBaseUrl = 'http://127.0.0.1:8088';
 final String clientId = '';
+final String clientSecret = '';
