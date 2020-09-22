f57facde175d23bf2e204dacdc474cebdc3c4951
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-22 22:29:13 +0800

    Create loading.dart

---
+++ lib/widgets/common/loading.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  final bool more;
+  Loading({this.more = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: more ? 20 : 100),
+        child: SizedBox(
+          width: 24,
+          height: 24,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
