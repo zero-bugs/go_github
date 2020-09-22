3f23f5c2650f4b9be7d004a6fec9472583718d94
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-22 22:38:53 +0800

    Create text_contains_organization.dart

---
+++ lib/widgets/text_contains_organization.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:gogithub/utils/global.dart';
+
+class TextContainsOrganization extends StatelessWidget {
+  final String text;
+  final TextStyle style;
+  final bool oneLine;
+
+  TextContainsOrganization(this.text, {this.style, this.oneLine = false});
+
+  static final _reg = RegExp(r'@[A-Za-z-]+');
+
+  @override
+  Widget build(BuildContext context) {
+    final orgs = _reg.allMatches(text).map((m) => m.group(0)).toList();
+    final chunks = text.split(_reg);
+
+    List<TextSpan> spans = [];
+    for (var index in List.generate(orgs.length, (i) => (i))) {
+      if (chunks[index].isNotEmpty) {
+        spans.add(TextSpan(text: chunks[index]));
+      }
+      spans.add(
+          createLinkSpan(context, orgs[index], '/' + orgs[index].substring(1)));
+    }
+    if (chunks.last.isNotEmpty) {
+      spans.add(TextSpan(text: chunks.last));
+    }
+
+    return Text.rich(
+      TextSpan(children: spans, style: style),
+      overflow: oneLine ? TextOverflow.ellipsis : TextOverflow.clip,
+      maxLines: oneLine ? 1 : null,
+    );
+  }
+}
