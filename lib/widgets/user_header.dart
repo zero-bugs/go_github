ead091fb9e8884559ade505ce53d30235ec7f7c9
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-22 22:39:25 +0800

    Create user_header.dart

---
+++ lib/widgets/user_header.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:gogithub/models/theme.dart';
+import 'package:gogithub/utils/global.dart';
+import 'package:provider/provider.dart';
+
+import 'avatar.dart';
+
+class UserHeader extends StatelessWidget {
+  final String avatarUrl;
+  final String name;
+  final String login;
+  final DateTime createdAt;
+  final String bio;
+  final Widget followWidget;
+
+  UserHeader({
+    @required this.avatarUrl,
+    @required this.name,
+    @required this.login,
+    @required this.createdAt,
+    @required this.bio,
+    this.followWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeModel>(context);
+    return Container(
+      padding: CommonStyle.padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Avatar(url: avatarUrl, size: AvatarSize.extraLarge),
+              if (followWidget != null) ...[
+                Expanded(child: Container()),
+                followWidget,
+              ]
+            ],
+          ),
+          SizedBox(height: 8),
+          if (name != null && name.isNotEmpty) ...[
+            Text(
+              name,
+              style: TextStyle(
+                color: theme.palette.text,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: 4),
+          ],
+          Text(
+            login,
+            style: TextStyle(
+              color: theme.palette.primary,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 8),
+          if (createdAt != null)
+            Row(
+              children: <Widget>[
+                Icon(
+                  Octicons.clock,
+                  size: 16,
+                  color: theme.palette.tertiaryText,
+                ),
+                SizedBox(width: 4),
+                Text(
+                  'Joined on ${dateFormat.format(createdAt)}',
+                  style: TextStyle(
+                    color: theme.palette.tertiaryText,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          if (bio != null && bio.isNotEmpty) ...[
+            SizedBox(height: 10),
+            Text(
+              bio,
+              style: TextStyle(
+                color: theme.palette.secondaryText,
+                fontSize: 17,
+              ),
+            )
+          ]
+        ],
+      ),
+    );
+  }
+}
