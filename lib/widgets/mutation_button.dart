9ef7df459bf3a0d4f1d5d0c9dffbe48c0f553316
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-22 22:36:49 +0800

    Create mutation_button.dart

---
+++ lib/widgets/mutation_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:gogithub/models/theme.dart';
+import 'package:provider/provider.dart';
+
+import 'link.dart';
+
+class MutationButton extends StatelessWidget {
+  final bool active;
+  final String text;
+  final VoidCallback onPressed;
+
+  MutationButton({
+    @required this.active,
+    @required this.text,
+    @required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeModel>(context);
+    final textColor = active ? theme.palette.background : theme.palette.primary;
+    final backgroundColor =
+        active ? theme.palette.primary : theme.palette.background;
+    return Link(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 4,
+        ),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          border: Border.all(color: theme.palette.primary),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 17, color: textColor),
+        ),
+      ),
+    );
+  }
+}
