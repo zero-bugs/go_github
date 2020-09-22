8f0ffb58f9eb3ffa7bc7ac5309ac1fd060891662
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-09-22 22:35:14 +0800

    Create issue_icon.dart

---
+++ lib/widgets/issue_icon.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:gogithub/utils/global.dart';
+
+enum IssueIconState {
+  open,
+  closed,
+  prOpen,
+  prClosed,
+  prMerged,
+}
+
+class IssueIcon extends StatelessWidget {
+  final IssueIconState state;
+  final double size;
+
+  IssueIcon(this.state, {this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case IssueIconState.open:
+        return Icon(Octicons.issue_opened,
+            color: GithubPalette.open, size: size);
+      case IssueIconState.closed:
+        return Icon(Octicons.issue_closed,
+            color: GithubPalette.closed, size: size);
+      case IssueIconState.prOpen:
+        return Icon(Octicons.git_pull_request,
+            color: GithubPalette.open, size: size);
+      case IssueIconState.prClosed:
+        return Icon(Octicons.git_pull_request,
+            color: GithubPalette.closed, size: size);
+      case IssueIconState.prMerged:
+        return Icon(Octicons.git_merge,
+            color: GithubPalette.merged, size: size);
+      default:
+        return null;
+    }
+  }
+}
